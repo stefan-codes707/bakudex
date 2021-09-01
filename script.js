edf8af5cd33793ed0bb6ec6984e98cc1f43c6598
Stefan-codes <contact@gmail.com>
2021-09-01 01:54:17 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,22 @@
+
+
+function coffeeOne(){
+	const circle = document.getElementById("circle").style.background = "#02643d";
+
+	const img = document.getElementById("starbucks-img");
+	img.src = "images/img1.png";
+}
+
+function coffeeTwo(){
+	const circle = document.getElementById("circle").style.background = "#E66484";
+
+	const img = document.getElementById("starbucks-img");
+	img.src = "images/img2.png";
+}
+
+function coffeeThree(){
+	const circle = document.getElementById("circle").style.background = "#8A275F";
+
+	const img = document.getElementById("starbucks-img");
+	img.src = "images/img3.png";
+}
